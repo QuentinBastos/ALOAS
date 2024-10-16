857fc5226671b9e6873e6f75cfcdd9792d24b068
p2110156 <quentin.bastos@etu.univ-lyon1.fr>
2024-10-16 12:20:29 +0200

    SAE-1 Mise en place de l'application

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+import './styles/app.scss';
+
+// assets/app.js
+import { createApp } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import './styles/app.scss';
+
+createApp(HelloWorld).mount('#app');
---
+++ assets/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<!-- assets/components/HelloWorld.vue -->
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+
+const message = ref('Hello, Vue 3!')
+</script>
+
+<style scoped>
+.hello {
+  color: blue;
+}
+</style>
