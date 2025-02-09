083b85c86580430bdbab06ba7406696a32637c9d
bastos <quentin.bastos@etu.univ-lyon1.fr>
2025-02-09 16:44:57 +0100

    feat(prod): change all db in postgreSQL

--- init.sh
+++ init.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
 echo "🔄 Starting initialization..."
 
 # Wait for PostgreSQL to be available
 echo "⏳ Checking PostgreSQL connection..."
-until PGPASSWORD="$DB_PASS" psql -h "$DB_HOST" -U "$DB_USER" -d "$POSTGRES_DB" -c "SELECT 1" &>/dev/null; do
+until PGPASSWORD="$DB_PASS" psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1" &>/dev/null; do
     >&2 echo "🔄 PostgreSQL is unavailable - sleeping"
     sleep 3
 done
 
 echo "✅ PostgreSQL is available!"
 
+# Generate migrations if needed
+echo "⚡ Generating PostgreSQL migrations..."
+php bin/console doctrine:migrations:diff || echo "No new migrations needed."
+
 # Run migrations
 echo "⚡ Running database migrations..."
-cd /var/www/html || exit
-php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
+php bin/console doctrine:migrations:migrate --no-interaction
+
 echo "✅ Migrations complete!"
-
-# Clear cache
-echo "🧹 Clearing cache..."
-php bin/console cache:clear
-
 echo "🎉 Initialization complete!"
