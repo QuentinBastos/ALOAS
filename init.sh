7196768360a34c5f1213cdb6de9315ca29209625
bastos <quentin.bastos@etu.univ-lyon1.fr>
2025-03-12 09:25:40 +0100

    fix(mysql): convert migration postgree to mysql

--- init.sh
+++ init.sh
@@ -13,7 +13,14 @@ done
 
 mysql -h db -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
-cd /var/www
+
+cd /var/www/html
+
+if [ ! -f bin/console ]; then
+  >&2 echo "❌ bin/console not found!"
+  exit 1
+fi
+
 php bin/console doctrine:migrations:migrate --no-interaction
 echo "✅ Migrations complete!"
 echo "🎉 Initialization complete!"
