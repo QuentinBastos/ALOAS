7fa10a6dc8fe48338e9f9e677c205515e0085222
bastos <quentin.bastos@etu.univ-lyon1.fr>
2025-03-13 17:08:40 +0100

    fix(htaccess): change

--- var/cache/prod/App_KernelProdContainer.php
+++ var/cache/prod/App_KernelProdContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelProdContainer::class, false)) {
 
 return new \ContainerMiv7J3S\App_KernelProdContainer([
     'container.build_hash' => 'Miv7J3S',
-    'container.build_id' => '01fceab2',
-    'container.build_time' => 1741873289,
+    'container.build_id' => 'c2f6fea5',
+    'container.build_time' => 1741882112,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerMiv7J3S');
